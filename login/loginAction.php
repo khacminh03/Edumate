73813cfed24cd4110f9ebc9da1c6b12e73b0df10
D4rU_Li3b3rt <122429060+khacminh03@users.noreply.github.com>
2024-08-19 15:03:17 +0700

    Add files via upload

---
+++ login/loginAction.php
@@ -0,0 +1,28 @@
+<?php
+    include '../libs/config.php';
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $sql = $conn->prepare("SELECT * FROM account WHERE username = ?");
+        $sql->bind_param("s", $username);
+        $sql->execute();
+        $result = $sql->get_result();
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            if (password_verify($password, $row["password"])) {
+
+                if ($row["accountid"] === "he") {
+                    header("Location: studentHome.html");
+                } else if ($row["accountid"] === "te") {
+                    header("Location: tutorHome.html");
+                } else {
+                    header("Location: adminHome.html");
+                }
+            } else {
+                echo "<script>alert('Wrong password or username')</script>";
+            }
+        } else {
+            echo "<script>alert('Wrong password or username')</script>";
+        }
+    }
+?>
